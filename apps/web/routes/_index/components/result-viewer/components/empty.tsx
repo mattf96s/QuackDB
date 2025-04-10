9fd52de4c58c927c4a3c0d26c008692f1af6a043
mattf96s <mattf96@gmail.com>
2025-04-10 12:04:45 +0200

    chore: move route files into app router

--- apps/web/routes/_index/components/result-viewer/components/empty.tsx
+++
@@ -1,7 +0,0 @@
-export default function EmptyResults() {
-  return (
-    <div className="flex size-full items-center justify-center">
-      <p className="text-gray-400">No query results</p>
-    </div>
-  );
-}
--- apps/web/routes/_index/components/sidepanel/components/wrapper/context/types.ts
+++
@@ -1,9 +0,0 @@
-import type { ImperativePanelHandle } from "react-resizable-panels";
-
-export type WrapperState = {
-  isCollapsed: boolean;
-  onToggleIsCollapse: (isCollapsed: boolean) => void;
-  ref: React.RefObject<ImperativePanelHandle>;
-  order: number;
-  id: string;
-};
